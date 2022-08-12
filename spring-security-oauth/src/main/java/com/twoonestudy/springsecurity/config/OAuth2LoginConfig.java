2e479eaa917936e4be2360061a70ef7990c23506
chenjujun <0431cjj@163.com>
2022-08-12 16:33:13 +0800

    授权认证分析

--- spring-security-oauth/src/main/java/com/twoonestudy/springsecurity/config/OAuth2LoginConfig.java
+++ spring-security-oauth/src/main/java/com/twoonestudy/springsecurity/config/OAuth2LoginConfig.java
@@ -11,37 +11,40 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
-public class OAuth2LoginConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.requestMatchers().mvcMatchers("/foo");
-        return http.build();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
-        http.requestMatchers().mvcMatchers("/bar");
-        return http.build();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(this.giteeClientRegistration());
-    }
-
-    private ClientRegistration giteeClientRegistration() {
-        return ClientRegistration.withRegistrationId("gitee")
-                .clientId("71d5c8b12c44ed568480413da7fe5403606666caa4c4807dcd7c8dd02bf676a1")
-                .clientSecret("876ae45f43efe1ad7ba6ed5b2e677ef7834c1986d21a2a5b65a969108a2ac48c")
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
-                .authorizationUri("https://gitee.com/oauth/authorize")
-                .tokenUri("https://gitee.com/oauth/token")
-                .userInfoUri("https://gitee.com/api/v5/user")
-                .userNameAttributeName("name")
-                .clientName("Gitee")
-                .build();
-    }
-}
+/**
+ * 覆盖yml配置
+ */
+//@Configuration
+//public class OAuth2LoginConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.requestMatchers().mvcMatchers("/foo");
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
+//        http.requestMatchers().mvcMatchers("/bar");
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository() {
+//        return new InMemoryClientRegistrationRepository(this.giteeClientRegistration());
+//    }
+//
+//    private ClientRegistration giteeClientRegistration() {
+//        return ClientRegistration.withRegistrationId("gitee")
+//                .clientId("71d5c8b12c44ed568480413da7fe5403606666caa4c4807dcd7c8dd02bf676a1")
+//                .clientSecret("876ae45f43efe1ad7ba6ed5b2e677ef7834c1986d21a2a5b65a969108a2ac48c")
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+//                .authorizationUri("https://gitee.com/oauth/authorize")
+//                .tokenUri("https://gitee.com/oauth/token")
+//                .userInfoUri("https://gitee.com/api/v5/user")
+//                .userNameAttributeName("name")
+//                .clientName("Gitee")
+//                .build();
+//    }
+//}
