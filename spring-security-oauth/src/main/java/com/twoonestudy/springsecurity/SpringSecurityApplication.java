2c5cee256c1db3ea0b71bb70a917f54cde9240f6
jujunChen <0431cjj@163.com>
2022-08-17 01:18:01 +0800

    授权认证服务器

--- spring-security-oauth/src/main/java/com/twoonestudy/springsecurity/SpringSecurityApplication.java
+++ spring-security-oauth/src/main/java/com/twoonestudy/springsecurity/SpringSecurityApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-//@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = true)
 @SpringBootApplication
 public class SpringSecurityApplication {
 
